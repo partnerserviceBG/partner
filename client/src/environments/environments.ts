d47ac25fec5ba58e5d0288761206c80ebe682bae
koporov_va <koporov_va@teleformis.ru>
2024-03-25 15:48:21 +0300

    test by deploy beget

--- client/src/environments/environments.ts
+++ client/src/environments/environments.ts
@@ -1,4 +1,3 @@
 export const environments = {
-  baseUrl:
-    process.env.NODE_ENV === 'production' ? 'https://partner-servicebg.ru/api/v1' : 'http://localhost:5000/api/v1',
+  baseUrl: 'https://partner-servicebg.ru/api/v1',
 };
